129606c2398c11112bc7b5e624190c978c4413c3
Ravibhaiya <rrtech684@gmail.com>
2026-07-19 20:01:03 +0530

    feat: add shimmer skeleton loading screen

--- lib/screens/reader_screen.dart
+++ lib/screens/reader_screen.dart
@@ -13,6 +13,7 @@ import 'package:taplingo/utils/js_injection.dart';
 
 import 'package:taplingo/widgets/manga_selection_overlay.dart';
 import 'package:taplingo/widgets/meaning_bottom_sheet.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
 
@@ -492,9 +493,7 @@ class _ReaderScreenState extends ConsumerState<ReaderScreen>
                       Positioned.fill(
                         child: Container(
                           color: Theme.of(context).scaffoldBackgroundColor,
-                          child: const Center(
-                            child: CircularProgressIndicator(),
-                          ),
+                          child: _buildSkeleton(context),
                         ),
                       ),
                   ],
@@ -586,6 +585,66 @@ class _ReaderScreenState extends ConsumerState<ReaderScreen>
               ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildSkeleton(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final baseColor = isDark ? Colors.grey[800]! : Colors.grey[300]!;
+    final highlightColor = isDark ? Colors.grey[700]! : Colors.grey[100]!;
+
+    return Shimmer.fromColors(
+      baseColor: baseColor,
+      highlightColor: highlightColor,
+      child: ListView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        padding: EdgeInsets.only(
+          top: MediaQuery.paddingOf(context).top + kToolbarHeight + 16,
+          left: 16,
+          right: 16,
+        ),
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          if (_isManga) {
+            // Skeleton for manga: large rectangular blocks mimicking image panels
+            return Container(
+              margin: const EdgeInsets.only(bottom: 16),
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+            );
+          } else {
+            // Skeleton for novel: paragraph text lines
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 16,
+                    width: double.infinity,
+                    color: Colors.white,
+                    margin: const EdgeInsets.only(bottom: 8),
+                  ),
+                  Container(
+                    height: 16,
+                    width: double.infinity,
+                    color: Colors.white,
+                    margin: const EdgeInsets.only(bottom: 8),
+                  ),
+                  Container(
+                    height: 16,
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
